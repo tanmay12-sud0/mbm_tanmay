cf7e0400cfce505dbb89cc80213f714ae1e470ae
member8lks <member8@lakshmisri.com>
2021-10-27 22:06:23 +0530

    header added

---
+++ components/UI/MenuList.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown, Button } from "react-bootstrap";
+import Link from "next/link";
+function MenuList({menus}) {
+  return (
+    <div>
+      <Navbar.Collapse id="navbarScroll">
+        <Nav
+          className="me-auto nav-menus my-2 my-lg-0 align-items-center"
+          style={{ maxHeight: "100px" }}
+          navbarScroll
+        >
+          <Link href="/"><Button variant="link" className="anchor-white">Home</Button></Link>
+          {
+            menus.map((item) => <>  
+                <NavDropdown
+                    key={item.type}
+                    id={item.type}
+                    className="header-menus-dropdown"
+                    title={
+                    <Button
+                        variant="link"
+                        className="p-1 anchor-white"
+                        onMouseEnter={() =>
+                        document.getElementById(item.type).click()
+                        }
+                    >
+                        {item.type}
+                    </Button>
+                    }
+                >
+                    {
+                        item.links.map((link) => (
+                           <NavDropdown.Item key={link}> <Link href={`/${link}`}><a className="anchor">{link}</a></Link></NavDropdown.Item>
+                        ))
+                    }
+                </NavDropdown>
+            </>)  
+          }
+        </Nav>
+      </Navbar.Collapse>
+    </div>
+  );
+}
+
+export default MenuList;
--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -1,14 +1,45 @@
-import React from 'react'
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 import Link from "next/link";
-export default function Header({menus}) {
-    return (
-        <>
-          <Head>
-            <title>MBM University</title>
-            <meta name="description" content="MBM University"/>
-          </Head>
-          <div>Header</div>
-        </>
-    )
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { IoIosMailOpen } from "react-icons/io";
+import MenuList from "../UI/MenuList";
+export default function Header({ menus }) {
+  return (
+    <>
+      <Head>
+        <title>MBM University</title>
+        <meta name="description" content="MBM University" />
+      </Head>
+      <Navbar className="mbm-main-header" expand="lg">
+        <Container className="header-container" fluid>
+        <div className="w-100 d-flex flex-wrap">
+          <Link href="/">
+            <Navbar.Brand
+              className="text-white d-flex cursor-pointer align-items-start font-weight-bold"
+            >
+              <img
+                className="mbm-logo mx-2"
+                src="https://mbm.ac.in/wp-content/uploads/2020/12/cropped-MBM-LOGO-80x80.png"
+              />
+              <div>
+                <h2 className="text-white">MBM University</h2>
+                <p className="m-0 font-weight-100 text-whitegray">
+                  Mugneeram Bangur Memorial Engineering College
+                </p>
+              </div>
+            </Navbar.Brand>
+          </Link>
+          <Navbar.Toggle className="m-auto" aria-controls="navbarScroll" />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text className="text-white d-flex align-items-center justify-content-center">
+              <IoIosMailOpen/> deanmbm@jnvu.edu.in
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </div>
+        <div className="w-100 text-center"><MenuList menus={menus}/></div>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
