c882d5fedc37aa61b07ebbb348b95a267729b889
Satwikan <satwikanmol25@gmail.com>
2021-10-27 17:51:47 +0530

    deleting react client

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { authReducer } from "./RolesAuth/authReducer";
-
-const rootReducer = combineReducers({
-    user: authReducer,
-})
-
-export default rootReducer;
