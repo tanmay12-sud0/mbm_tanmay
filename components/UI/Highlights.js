5ae28bddb094c83f5caa41d43f34d16a4ce6cf22
Satwikan <satwikanmol25@gmail.com>
2021-10-28 18:37:59 +0530

    added some content to hightlights

--- components/UI/Highlights.js
+++ components/UI/Highlights.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, CardGroup, Row, Col } from "react-bootstrap";
 import { useMediaQuery } from "react-responsive";
 
 const Highlights = () => {
@@ -13,12 +13,33 @@ const Highlights = () => {
     },
   };
   return (
-    <>
-      <Card style={style.container}>
+    <CardGroup style={style.container}>
+      <Row xs={1} md={1} className="g-4">
         <Card.Header>Highlights</Card.Header>
-        <Card.Body>MBM University is launching a new website</Card.Body>
-      </Card>
-    </>
+        <Col>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://imgr.search.brave.com/LEQ03RgAKidYivi-FI-gByRpzEeTHpQLsV_pQoE_BLY/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5N/dkY2QnhTSkdlZVdr/N3JTOF9zdnFRSGFF/OCZwaWQ9QXBp"
+            />
+            <Card.Title>New Website</Card.Title>
+            <Card.Body>MBM University is launching a new website</Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://imgr.search.brave.com/xmsRvhfAwwP1CUZiJuXfF1Xl9rLFaXB1Vw0l9LaW0Yw/fit/775/400/ce/1/aHR0cHM6Ly9zZW9m/aWxlcy5zMy5hbWF6/b25hd3MuY29tL3Nl/by9tZWRpYS91cGxv/YWRzLzIwMTgvMDgv/MTAvcGxhY2VtZW50/LW9mZmljZXItb3It/cGxhY2VtZW50LWNl/bGwuanBn"
+            />
+            <Card.Title>Placement</Card.Title>
+            <Card.Body>
+              MBM University Is above all IITs - UNO{" "}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </CardGroup>
   );
 };
 
