190b1575112e2cc2eb4ee89f697ee16621b774ed
Satwikan <satwikanmol25@gmail.com>
2021-10-30 09:29:46 +0530

    added more contetn

--- components/UI/MainBody.js
+++ components/UI/MainBody.js
@@ -2,7 +2,30 @@ import React from "react";
 import { Card } from "react-bootstrap";
 function MainBody() {
   return (
-    <Card style={{ width: "100%" , padding:"20px"}}>
+    <Card style={{ width: "100%", padding: "20px", textAlign: "center" }}>
+      <h2>Where does it come from?</h2>
+      <p>
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC, making it
+        over 2000 years old. Richard McClintock, a Latin professor at
+        Hampden-Sydney College in Virginia, looked up one of the more obscure
+        Latin words, consectetur, from a Lorem Ipsum passage, and going through
+        the cites of the word in classical literature, discovered the
+        undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
+        of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
+        Cicero, written in 45 BC. This book is a treatise on the theory of
+        ethics, very popular during the Renaissance. The first line of Lorem
+        Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
+        1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is
+        reproduced below for those interested. Sections 1.10.32 and 1.10.33 from
+        "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their
+        exact original form, accompanied by English versions from the 1914
+        translation by H. Rackham.
+      </p>
+      <img
+        src="https://imgr.search.brave.com/_vbNjlk8qkT6Z2BVqFMZ88LU-tIKqlMB61o0QaOOjPo/fit/920/225/ce/1/aHR0cHM6Ly90c2U0/LmV4cGxpY2l0LmJp/bmcubmV0L3RoP2lk/PU9JUC5Nc1p0elRv/M2dwOE1vcEFpejFU/R0xnSGFEMCZwaWQ9/QXBp"
+        alt=" placement"
+      />
       <p>
         At unde quaerat ipsa magnam iste vel perferendis corrupti facilis solut
         aaaaa delectus laboriosam, voluptatum deserunt enim cumque id vitae cum
