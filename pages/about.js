b7cc158f6e0e7ccc1ed1e1fe4db12c63cced3dfd
Vidhi Sharma <vidhisharma6658@gmail.com>
2021-12-05 17:32:21 +0530

    cssPull

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@
 // import Highlights from "../components/UI/Highlights";
 // import Layout from "../components/layouts";
 import MainBody from "../components/UI/MainBody";
+import "../styles/about.css";
 
 // import Links from "../components/UI/Links";
 import BreadcrumbComponent from "../components/UI/CustomBreadcrumb";
@@ -9,7 +10,7 @@ import RelatedLinks from "../components/UI/RelatedLinks";
 function Home() {
   return (
     <>
-      <div className="Home-content home-display" style="">
+      <div className="Home-content home-display">
         <BreadcrumbComponent />
         <div className="m-md-5 m-2">
           <div className="row d-flex mr-5 ml-5">
