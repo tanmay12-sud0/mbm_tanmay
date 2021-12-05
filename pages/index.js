c4337d88b197d66e8c487aaa2bbb08a3032a6384
anurag <anuraggupta3979@gmail.com>
2021-12-05 15:27:58 +0530

    vc component

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,28 @@
 import React from "react";
+
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
 function home() {
   return (
     <div>
       <div className="container">
         <div className="row">
-          <div className="col-5">
+          <div className="col-md-5 col-12">
             <div>
               <img
                 style={{ border: "3px solid rgba(55,63,80,1)" }}
                 src="https://th.bing.com/th/id/OIP.wUk9lMqzmr9pthy8K3NHGwHaE2?pid=ImgDet&rs=1"
                 alt=""
+                className="card-img img-responsive"
               />
             </div>
           </div>
-          <div className="col-7">
+          <div className="col-md-7 col-12 mt-md-0 mt-4">
             <h1>
               <span
                 style={{
@@ -33,7 +42,7 @@ function home() {
                 UNIVERSITY
               </span>
             </h1>
-            <p style={{ color: "black" }}>
+            <p style={{ color: "black", textAlign: "justify" }}>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
               sit sed, architecto consequuntur eveniet laudantium eum rerum,
               aliquid soluta doloremque ipsa beatae provident quis distinctio
@@ -79,6 +88,166 @@ function home() {
                     &gt; Frequent Question
                   </li>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row" style={{ marginTop: "100px" }}>
+          <div className="col-md-8 col-12">
+            <h1 style={{ marginBottom: "30px" }}>
+              <span
+                style={{
+                  fontSize: "1.8rem",
+                  fontWeight: "bold",
+                }}
+              >
+                From{" "}
+              </span>
+              <span
+                style={{
+                  fontSize: "1.8rem",
+                  fontWeight: "bold",
+                  color: "red",
+                }}
+              >
+                Vice Chancellor's
+              </span>{" "}
+              <span
+                style={{
+                  fontSize: "1.8rem",
+                  fontWeight: "bold",
+                }}
+              >
+                Desk
+              </span>
+            </h1>
+
+            <div className="col-md-12 col-12">
+              <div className="row d-flex">
+                <div className="col-md-5 col-12">
+                  <Avatar
+                    style={{
+                      textAlign: "center",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      margin: "auto",
+                      border: "2px solid #ef2727",
+                    }}
+                    alt="Remy Sharp"
+                    src="https://th.bing.com/th/id/OIP.wUk9lMqzmr9pthy8K3NHGwHaE2?pid=ImgDet&rs=1"
+                    sx={{ width: 150, height: 150 }}
+                  />
+                  <div
+                    style={{
+                      textAlign: "center",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      margin: "auto",
+                    }}
+                  >
+                    <p
+                      style={{
+                        fontSize: "15px",
+                        color: "red",
+                        marginBottom: "2px",
+                        marginTop: "6px",
+                      }}
+                    >
+                      Prof. Ajay Kumar Sharma
+                    </p>
+                    <p
+                      style={{
+                        fontSize: "12px",
+                        marginBottom: "1px",
+                        marginTop: "0px",
+                      }}
+                    >
+                      Hon'ble Vice Chancellor
+                    </p>
+                    <p
+                      style={{
+                        fontSize: "12px",
+                        marginBottom: "1px",
+                        marginTop: "0px",
+                      }}
+                    >
+                      MBM UNIVERSITY
+                    </p>
+                    <p
+                      style={{
+                        fontSize: "12px",
+                        marginBottom: "0px",
+                        marginTop: "0px",
+                      }}
+                    >
+                      Jodhpur, Rajasthan
+                    </p>
+                  </div>
+                </div>
+                <div className="col-md-6 col-12">
+                  <p style={{ textAlign: "justify" }}>
+                    Lorem ipsum dolor sit amet consectetur ipsum eius vitae sint
+                    id autem molestias asperiores, iste at voluptate sapiente
+                    odio provident?
+                  </p>
+                  <p style={{ textAlign: "justify" }}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Voluptates exercitationem quos officia quo, harum debitis
+                    Voluptates exercitationem quos officia quo, harum debitis
+                    Dignissimos a, nostrum perferendis non vero tempore!
+                  </p>
+                  <p style={{ textAlign: "justify" }}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Laudantium illum ducimus iusto, quod obcaecati blanditiis.
+                    Laudantium illum ducimus iusto, quod obcaecati blanditiis.
+                    Veritatis nulla corrupti quisquam dolor assumenda maiores
+                    omnis, voluptatum officia!
+                  </p>
+                </div>
+                <div className="col-md-1 col-12"></div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 col-12">
+            <div>
+              <div>
+                <h1 style={{ marginBottom: "30px" }}>
+                  <span
+                    style={{
+                      fontSize: "1.8rem",
+                      fontWeight: "bold",
+                      color: "red",
+                    }}
+                  >
+                    Recent
+                  </span>{" "}
+                  <span
+                    style={{
+                      fontSize: "1.8rem",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Highlights
+                  </span>
+                </h1>
+                <Card sx={{ width: "100%" }}>
+                  <CardMedia
+                    style={{
+                      border: "5px solid #ef2727",
+                    }}
+                    component="img"
+                    height="250"
+                    image="https://th.bing.com/th/id/OIP.wUk9lMqzmr9pthy8K3NHGwHaE2?pid=ImgDet&rs=1"
+                    alt="green iguana"
+                  />
+                  <CardContent style={{ backgroundColor: "#ef2727" }}>
+                    <Typography variant="body2" color="white">
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </Card>
               </div>
             </div>
           </div>
