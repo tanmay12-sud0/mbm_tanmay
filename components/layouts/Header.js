d7d05d0e24ed60304dcde13afcd0f98b44729b47
Satwikan <satwikanmol25@gmail.com>
2021-12-05 16:09:48 +0530

    contact modal

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -1,14 +1,43 @@
 import React from "react";
 import Head from "next/head";
 import Link from "next/link";
-import { Navbar, Container } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 import { IoIosMailOpen } from "react-icons/io";
 import MenuList from "../UI/MenuList";
 import PageTitle from "../UI/PageTitle";
 import RoomIcon from "@mui/icons-material/Room";
 import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Header({ menus, globalVariables }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const ContactPopup = ({ text }) => {
+    return (
+      <>
+        <button className="header-contact-popup-button" onClick={handleShow}>
+          {text}
+        </button>
+        <Modal show={show} onHide={handleClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  };
   return (
     <div
       style={{
@@ -38,8 +67,15 @@ export default function Header({ menus, globalVariables }) {
         </div>
         <div style={{ display: "flex", flexFlow: "wrap" }}>
           <a href="#!" className="header-contact-link">
-            <ContactPhoneIcon />
-            Contact Information
+            {
+              <ContactPopup
+                text={
+                  <>
+                    <ContactPhoneIcon /> Contact Information
+                  </>
+                }
+              />
+            }
           </a>
           {"  |  "}
           <a href="#!" className="header-contact-link">
