5639aceaa4c1848da97645065a402a74a0473ada
Satwikan <satwikanmol25@gmail.com>
2021-12-05 16:12:46 +0530

    removed modal footer

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -26,14 +26,6 @@ export default function Header({ menus, globalVariables }) {
             <Modal.Title>Modal heading</Modal.Title>
           </Modal.Header>
           <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
         </Modal>
       </>
     );
