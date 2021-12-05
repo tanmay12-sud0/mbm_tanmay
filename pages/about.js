ae78cec7c406505e0551944171dac62419916fd4
Vidhi Sharma <vidhisharma6658@gmail.com>
2021-12-05 15:58:29 +0530

    csCchanges

--- pages/about.js
+++ pages/about.js
@@ -2,13 +2,14 @@
 // import Highlights from "../components/UI/Highlights";
 // import Layout from "../components/layouts";
 import MainBody from "../components/UI/MainBody";
+
 // import Links from "../components/UI/Links";
 import BreadcrumbComponent from "../components/UI/CustomBreadcrumb";
 import RelatedLinks from "../components/UI/RelatedLinks";
 function Home() {
   return (
     <>
-      <div className="Home-content home-display">
+      <div className="Home-content home-display" style="">
         <BreadcrumbComponent />
         <div className="m-md-5 m-2">
           <div className="row d-flex mr-5 ml-5">
