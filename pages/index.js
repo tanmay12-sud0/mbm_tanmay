24458798aecd30db23b1aaa9d809f1bb141fb492
anurag <anuraggupta3979@gmail.com>
2021-12-06 15:48:11 +0530

    header css correction

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,4 @@
 import React from "react";
-
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import Avatar from "@mui/material/Avatar";
 import HeaderCarousel from "../components/ui/HeaderCarousel";
 import Vcdesk from "../components/UI/Vcdesk";
 import Mbmintro from "../components/UI/Mbmintro";
