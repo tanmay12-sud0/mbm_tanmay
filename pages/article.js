4008918d2beb9d49b06fe066e7a0b663da104701
Satwikan <satwikanmol25@gmail.com>
2021-12-06 12:00:36 +0530

    article card in article page

--- pages/article.js
+++ pages/article.js
@@ -2,7 +2,11 @@ import React from "react";
 import ArticleSingle from "../components/ui/ArticleSingle";
 import BreadcrumbComponent from "../components/ui/CustomBreadcrumb";
 import Button from "@mui/material/Button";
+import ArticleCard from "../components/ui/ArticleCard";
+
 function article() {
+  const data =
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, corrupti cum. Hic, quis? Voluptates facere ";
   return (
     <div>
       <BreadcrumbComponent />
@@ -79,6 +83,30 @@ function article() {
         </p>
       </div>
       <hr />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          paddingLeft: "7vw",
+          paddingRight: "7vw",
+        }}
+      >
+        <h2>More From #Announcements</h2>
+        <a href="/announcements" style={{ color: "rgba(55,63,80,1)" }}>
+          See all
+        </a>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexFlow: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        <ArticleCard desc={data} />
+        <ArticleCard desc={data} />
+        <ArticleCard desc={data} />
+      </div>
     </div>
   );
 }
