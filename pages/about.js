5c6f010602cde20bef012b2442bcf4faaf399a6f
Satwikan <satwikanmol25@gmail.com>
2021-12-06 12:08:40 +0530

    about page article card

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,11 @@ import MainBody from "../components/ui/MainBody";
 // import Links from "../components/UI/Links";
 import BreadcrumbComponent from "../components/ui/CustomBreadcrumb";
 import RelatedLinks from "../components/ui/RelatedLinks";
+import ArticleCard from "../components/ui/ArticleCard";
+
 function Home() {
+  const data =
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, corrupti cum. Hic, quis? Voluptates facere ";
   return (
     <>
       <div className="Home-content home-display">
@@ -15,10 +19,35 @@ function Home() {
         <div className="m-md-5 m-2">
           <div className="row d-flex mr-5 ml-5">
             <div className="d-flex  col-12">
-              <RelatedLinks></RelatedLinks>
+              <RelatedLinks />
               <MainBody />
             </div>
           </div>
+        </div>
+        <hr />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            paddingLeft: "7vw",
+            paddingRight: "7vw",
+          }}
+        >
+          <h2>Latest Updates</h2>
+          <a href="/announcements" style={{ color: "rgba(55,63,80,1)" }}>
+            See all Updates
+          </a>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexFlow: "wrap",
+            justifyContent: "center",
+          }}
+        >
+          <ArticleCard desc={data} />
+          <ArticleCard desc={data} />
+          <ArticleCard desc={data} />
         </div>
       </div>
     </>
