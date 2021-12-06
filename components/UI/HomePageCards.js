6b81313c61c1c2d5eed4a446fa9e4ab69c20ae0b
anurag <anuraggupta3979@gmail.com>
2021-12-06 21:28:32 +0530

    announcement css

--- components/UI/HomePageCards.js
+++ components/UI/HomePageCards.js
@@ -1,56 +1,97 @@
-import React from 'react'
-import { Card, Col, Container, Row, Button } from 'react-bootstrap'
-import ArticleCard from './ArticleCard'
+import React from "react";
+import { Card, Col, Container, Row, Button } from "react-bootstrap";
+import ArticleCard from "./ArticleCard";
 
 function HomePageCards() {
-    return (
-        <>
-        <br />
-          <br />
-          <br />
-        <div >
-           <Container>
-               <h1 style={{fontSize:"40px", color:"red"}}>
-                LATEST NEWS
-            </h1>
-            
-           </Container>
-          <Container>
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      <div>
+        <Container>
+          <h1 style={{ fontSize: "40px", color: "red" }}>LATEST NEWS</h1>
+        </Container>
+        <Container>
           <Row>
-        
-        <Col>
-        <ArticleCard desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illum ducimus iusto, quod obcaecati blanditiis. Laudantium illum ducimus iusto, quod obcaecati blanditiis. Veritatis nulla corrupti quisquam dolor assumenda maiores omnis, voluptatum officia" key={1} />
-        
-        </Col>
-        <Col>
-        <ArticleCard desc ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illum ducimus iusto, quod obcaecati blanditiis. Laudantium illum ducimus iusto, quod obcaecati blanditiis. Veritatis nulla corrupti quisquam dolor assumenda maiores omnis, voluptatum officia" key={2}/>
-        </Col>
-        <Col>
-        <div >
-        <Card  style={{ width: '18rem', border:"1px solid black" }}>
-        <Card.Title style={{display:"flex", justifyContent:"center", color:"red", fontSize:"30px", marginTop:"10px" }}>Campus highlights</Card.Title>
+            <Col>
+              <ArticleCard
+                desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illum ducimus iusto, quod obcaecati blanditiis. Laudantium illum ducimus iusto, quod obcaecati blanditiis. Veritatis nulla corrupti quisquam dolor assumenda maiores omnis, voluptatum officia"
+                key={1}
+              />
+            </Col>
+            <Col>
+              <ArticleCard
+                desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illum ducimus iusto, quod obcaecati blanditiis. Laudantium illum ducimus iusto, quod obcaecati blanditiis. Veritatis nulla corrupti quisquam dolor assumenda maiores omnis, voluptatum officia"
+                key={2}
+              />
+            </Col>
+            <Col>
+              <div>
+                <Card style={{ width: "18rem", border: "1px solid black" }}>
+                  <Card.Title
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      color: "red",
+                      fontSize: "30px",
+                      marginTop: "10px",
+                    }}
+                  >
+                    Campus highlights
+                  </Card.Title>
+                  <hr />
+                  <Card.Img
+                    variant="top"
+                    src="http://mbm.ac.in/wp-content/uploads/2021/08/mbm_front.png"
+                  />
+                  <Card.Body>
+                    <Card.Title
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        color: "red",
+                        fontSize: "20px",
+                        marginTop: "5px",
+                      }}
+                    >
+                      Mr. Andrew Mied
+                    </Card.Title>
+                    <Card.Title
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        color: "black",
+                        fontSize: "12px",
+                        marginTop: "5px",
+                      }}
+                    >
+                      BE Second Year, CSE
+                    </Card.Title>
+                    <hr style={{ color: "red" }} />
+                    <Card.Text
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        fontSize: "13px",
+                      }}
+                    >
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Laudantium illum ducimus iusto, quod obcaecati blanditiis.
+                      Laudantium illum ducimus iusto,
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        <br />
         <hr />
-  <Card.Img variant="top" src="http://mbm.ac.in/wp-content/uploads/2021/08/mbm_front.png" />
-  <Card.Body>
-    <Card.Title style={{display:"flex", justifyContent:"center", color:"red", fontSize:"20px", marginTop:"5px" }} >Mr. Andrew Mied</Card.Title>
-    <Card.Title style={{display:"flex", justifyContent:"center", color:"black", fontSize:"12px", marginTop:"5px" }} >BE Second Year, CSE</Card.Title>
-    <hr style={{color:"red"}} />
-    <Card.Text style={{display:"flex", justifyContent:"center", alignItems:"center", fontSize:"13px"}}>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illum ducimus iusto, quod obcaecati blanditiis. Laudantium illum ducimus iusto,
-    </Card.Text>
-    
-  </Card.Body>
-</Card>
-        </div>
-        
-        </Col>
-      </Row>
-          </Container>
-          <br />
-          <hr />
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
-export default HomePageCards
+export default HomePageCards;
--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,10 @@ import abc1 from "../images/abc1.png";
 import abc2 from "../images/abc.png";
 import abc3 from "../images/abc3.png";
 import HomePageCards from "../components/UI/HomePageCards";
-import Vcdesk from '../components/UI/Vcdesk';
-import Mbmintro from '../components/UI/Mbmintro';
+import Vcdesk from "../components/UI/Vcdesk";
+import Mbmintro from "../components/UI/Mbmintro";
 import HomePagePro from "../components/UI/HomePagePro";
+import YouAreLookingFor from "../components/UI/YouAreLookingFor";
 function home() {
   return (
     <div>
@@ -15,6 +16,7 @@ function home() {
         <Mbmintro></Mbmintro>
         <Vcdesk />
       </div>
+      <YouAreLookingFor></YouAreLookingFor>
       <HomePageCards />
       <HomePagePro />
     </div>
